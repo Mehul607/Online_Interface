c0a56351b7ab070744dd322ac92eca66460138f5
Mehul607 <mehul607@gmail.com>
2015-10-22 23:28:16 +0530

    Duplicate words dealt with

--- analyser/views.py
+++ analyser/views.py
@@ -13,10 +13,14 @@ def mainS(request):
         s=Sentence(stext=form_input)
         for (a,b) in tempSentence:
             l=Language.objects.filter(language__startswith=b)
-
             w=Word(wtext=a,lang=l[0])
             wlist.append((a,l[0].colour.col,l[0].language))
-            w.save()
+            tempw=Word.objects.filter(wtext__startswith=a)
+            for wt in tempw:
+                if w.lang.language==wt.lang.language:
+                    break
+            else:
+                w.save()
         s.save()
         return render(request, 'Online_Interface/final.html',{'word':wlist,})
     else:
